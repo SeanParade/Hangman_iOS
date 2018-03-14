77ff485c34156c8556467a68792df567fb2e0ff4
Tech <tech@Techs-iMac-46.local>
2018-03-14 12:31:53 -0400

    outlet vars created for gameboard

--- iHang/iHang/ViewController.swift
+++ iHang/iHang/ViewController.swift
@@ -9,6 +9,18 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    //display You Win/You Lose! && change text color
+    @IBOutlet weak var winLabel: UILabel!
+    
+    //view to hold labels: labels gen based on word length
+    @IBOutlet weak var guessView: UIStackView!
+    
+    //view that holds cells with keyboard buttons
+    @IBOutlet weak var keyboardView: UICollectionView!
+    
+    //click to change view controller -> About
+    @IBOutlet weak var infoBtn: UIButton!
 
     override func viewDidLoad() {
         super.viewDidLoad()
