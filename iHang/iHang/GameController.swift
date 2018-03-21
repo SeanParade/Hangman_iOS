d78e1f1d5429d3bb15119aa0859a0ba54e39eac4
Sean Price <SeanParade@users.noreply.github.com>
2018-03-21 13:37:51 -0400

    Couple Updates to game controller

--- iHang/iHang/GameController.swift
+++ iHang/iHang/GameController.swift
@@ -15,57 +15,56 @@ class GameController: UIViewController{
     
     @IBOutlet weak var testLabel: UILabel!
     
-    //where hangman will be drawn
+    // where hangman will be drawn
     @IBOutlet weak var hangmanView: UIView!
     
-    //view for labels : generated # depending on word
+    // view for labels : generated # depending on word
     @IBOutlet weak var guessView: UIStackView!
     
-    //display win/lose text : change color to green/red
+    // display win/lose text : change color to green/red
     @IBOutlet weak var winLabel: UILabel!
     
     var wordList:[String] = []
     var currentWord: String = ""
     var numGuesses: Int = 6
     var numBlanks: Int = 6
+    var gameOver = false;
     
     
-    
-    //click action for all buttons in keyboard
-    //log which letter clicked && change text color && disable button
+    // click action for all buttons in keyboard
+    // log which letter clicked && change text color && disable button
     @IBAction func letterClick(_ sender: UIButton) {
-       //show letter that is clicked in test label
-        testLabel.text = sender.currentTitle;
+       
+        if !gameOver {
+       // show letter that is clicked in test label
+        testLabel.text?.append(sender.currentTitle!);
         
         let char = Character(sender.currentTitle!)
         let indexes = getIndexesOf(letter: char)
         
-        if indexes.count != 0{
-            //make button green
+        if indexes.count != 0 {
+            // make button green
             sender.backgroundColor = UIColor.green
             
-            //set labels with button text
+            // set labels with button text
             for index in indexes{
                 if let label = self.view.viewWithTag(index) as? UILabel{
-                    label.text = sender.currentTitle
-                    numBlanks = numBlanks-1
+                    label.text = sender.currentTitle;
+                    numBlanks = numBlanks-1;
                 }
             }
-            
         }
         else{
-            //make button red
+            // make button red
             sender.backgroundColor = UIColor.red
-            //draw hangman vector
+            // draw hangman vector
             numGuesses = numGuesses-1
         }
-        //disable button
-        sender.isEnabled = false
-        
-        checkGameOver()
+        // disable button
+        sender.isEnabled = false;
+        checkGameOver();
+     }
     }
-        
-    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -81,22 +80,24 @@ class GameController: UIViewController{
         super.viewWillAppear(animated)
         let wp = WordProvider();
         wordList = wp.wordList;
-        //if there's no word, choose a random one
+        // if there's no word, choose a random one
         if(currentWord == "")
         {
          getRandomWord()
         }
-        //number of empty spaces
+        // number of empty spaces
         numBlanks = currentWord.count
-        setLabelTags()
+        setLabelTags();
     }
     
+    // Get random word from the word list
     func getRandomWord(){
         let len = UInt32(wordList.count-1)
         let randomInt = Int(arc4random_uniform(len))
         currentWord = wordList[randomInt]
     }
-    //returns an array of integers where character is present in word
+    
+    // returns an array of integers where character is present in word
     func getIndexesOf(letter: Character)->[Int]{
         var indexes: [Int] = [Int]()
         var count = 0
@@ -108,20 +109,21 @@ class GameController: UIViewController{
         }
         return indexes;
     }
+    
     //checks if no more guesses, no more blanks
     func checkGameOver()->(){
         if numGuesses == 0{
             winLabel.textColor = UIColor.red
             winLabel.text = "You Lose!"
+            gameOver = true;
         }
         else if numBlanks == 0{
             winLabel.textColor = UIColor.green
             winLabel.text = "You Win!"
-            
+            gameOver = true
         }
-    
-        
     }
+    
     //generates labels for each letter in the current word
     func setLabelTags(){
         for index in 0...currentWord.count{
@@ -134,7 +136,10 @@ class GameController: UIViewController{
     //func reset()
     //move initial values to here and call on button press or when view is first loaded
     
-    
+    func newGame() ->(){
+        winLabel.text = "";
+        numGuesses = 6
+        numBlanks = 6
+        gameOver = false
+    }
 }
-
-
